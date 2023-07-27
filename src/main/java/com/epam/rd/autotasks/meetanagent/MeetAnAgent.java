af5c94976f93a48f227d986498fc41bca4b1b040
Marco Antonio Velázquez Velázquez <velazquezv.marco@gmail.com>
2023-07-27 13:52:16 -0600

    Solved

--- src/main/java/com/epam/rd/autotasks/meetanagent/MeetAnAgent.java
+++ src/main/java/com/epam/rd/autotasks/meetanagent/MeetAnAgent.java
@@ -1,9 +1,22 @@
 package com.epam.rd.autotasks.meetanagent;
 
+import java.util.Scanner;
+
 public class MeetAnAgent {
     final static int PASSWORD = 133976; //You can change pass, but don't change the type
 
     public static void main(String[] args) {
         //put you code here
+
+        Scanner input = new Scanner(System.in);
+        /*System.out.println("Enter your password: ");*/
+        int number = input.nextInt();
+
+        if (number == PASSWORD){
+            System.out.println("Hello, Agent");
+        }
+        else {
+            System.out.println("Access denied");
+        }
     }
 }
